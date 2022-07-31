08c933763bd490c544b4971ea126139d571477c6
Your Name <you@example.com>
2022-07-31 22:43:29 +0300

    добавил тесты

--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -5,11 +5,28 @@ import ru.netology.sqr.javaqamvnpackage.services.SQRService;
 public class SQRServiceTest {
 
     @Test
-    public void calcsquares() {
+    public void calcsquares15() {
         SQRService service = new SQRService();
 
         int expected = 15;
         int actual = service.calcsquares(200, 300);
         Assertions.assertEquals(expected, actual);
     }
+
+//         @Test
+//    public void calcsquares16() {
+//        SQRService service = new SQRService();
+//
+//        int expected = 16;
+//        int actual = service.calcsquares(200, 300);
+//        Assertions.assertEquals(expected, actual);
+//    }
+//    @Test
+//    public void calcsquares17() {
+//        SQRService service = new SQRService();
+//
+//        int expected = 16;
+//        int actual = service.calcsquares(200, 300);
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
